bea9620d7eb1e7338e8439d7d7e5ab7d0f971d26
Ghoya <canadamubarak1@gmail.com>
2020-12-17 23:20:08 -0800

    another initial git ignore practice

---
+++ src/main/java/gitPractice/gitP.java
@@ -0,0 +1,9 @@
+package gitPractice;
+
+public class gitP {
+    public static void main(String[] args) {
+
+
+        System.out.println("\"Hello Git Last Day\" = " + "Hello Git Last Day");
+    }
+}
